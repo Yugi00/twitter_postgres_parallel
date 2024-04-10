5b8ee481f700f32945deb6a52cf40229eded469b
Yugi00 <jyep09@gmail.com>
2024-04-09 23:32:22 -0700

    fixed parallel

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -12,10 +12,10 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
-time echo "$files" | parallel sh load_normalized.sh
+time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:1306/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
-time echo "$files" | parallel sh load_normalized_batch.sh
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1307/ --inputs
